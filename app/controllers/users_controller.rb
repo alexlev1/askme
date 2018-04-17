55cd516bc0d25345873beb5be7bd09fdfdd35db3
Alex Levashov <alevash1@gmail.com>
2018-04-17 16:59:09 +0300

    Change routes for User, change Users Controller and View for action new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,15 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Alex',
-        username: 'lev',
-        avatar_url: 'http://cv.alevashov.ru/images/avatar.png',
-      ),
-      User.new(
-        id: 2,
-        name: 'George',
-        username: 'goga'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
 
   def show
-    @user = User.new(
-      name: 'Alex',
-      username: 'lev',
-      avatar_url: 'http://cv.alevashov.ru/images/avatar.png',
-    )
-
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('13.04.2018')),
-      Question.new(text: 'На работу пойдём?', created_at: Date.parse('15.04.2018')),
-      Question.new(text: 'В чём смысл жизни?', created_at: Date.parse('16.04.2018')),
-      Question.new(text: 'Что со мной не так?', created_at: Date.parse('17.04.2018')),
-      Question.new(text: 'КТО ЗДЕСЬ?!!', created_at: Date.parse('15.04.2018'))
-    ]
-
-    @new_question = Question.new
   end
 end
