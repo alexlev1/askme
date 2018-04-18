693cedb8921937700988fc1090b28a45d0b6c1fb
Alex Levashov <alevash1@gmail.com>
2018-04-18 23:16:14 +0300

    Add error message in new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
 
     if @user.save
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
     end
   end
 
@@ -19,6 +21,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    
   end
 
   private
