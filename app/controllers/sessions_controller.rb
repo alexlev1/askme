4c5efd19ecf1027adffab5d20155965b8bc1aa6c
Alex Levashov <alevash1@gmail.com>
2018-04-21 13:53:07 +0300

    Add session controller functional, add authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.authenticate(params[:email], params[:password])
+
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'вы успешно залогинились'
+    else
+      flash.now.alert = 'Неправильный email или пароль'
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Вы разлогинились. Приходите ещё!'
   end
 end
