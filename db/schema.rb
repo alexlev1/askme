3edc3fbda55717408f67fdefd79be0d16a847cd3
Alex Levashov <alevash1@gmail.com>
2018-04-25 20:16:03 +0300

    Change DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424203832) do
+ActiveRecord::Schema.define(version: 20180423182027) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
