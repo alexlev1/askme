61da5f326ee4fef5eaa42bdd677eb710dadd271e
Alex Levashov <alevash1@gmail.com>
2018-05-20 14:05:39 +0300

    Security fix for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, except: [:destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
-
-  get 'sign_up', to: 'users#new'
-  get 'log_out', to: 'sessions#destroy'
-  get 'log_in', to: 'sessions#new'
 end
