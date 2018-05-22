b309fa94926b7f93315074c7a0651c866ff432af
Alex Levashov <alevash1@gmail.com>
2018-05-22 21:00:35 +0300

    Add reCaptcha for unauthorized users

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,7 +11,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.author = current_user if current_user.present?
 
-    if @question.save
+    if check_captcha(@question) && @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
       render :edit
@@ -64,5 +64,11 @@ class QuestionsController < ApplicationController
     end
   end
 
-
+  def check_captcha(model)
+    if current_user.present?
+      true
+    else
+      verify_recaptcha(model: model)
+    end
+  end
 end
